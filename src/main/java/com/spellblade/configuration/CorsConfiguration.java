97211af38b1de8c22d4345f07b0b5834431decd2
bobulousness <bobulousness@gmail.com>
2026-06-28 13:51:46 -0400

    cors 3

--- src/main/java/com/spellblade/configuration/CorsConfiguration.java
+++ src/main/java/com/spellblade/configuration/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all API paths
-                .allowedOrigins("http://localhost:3000", "104.139.185.123:3000", "https://main.dgyamk5jkumb2.amplifyapp.com:3000")
+                .allowedOrigins("http://localhost:3000", "104.139.185.123:3000", "main.dgyamk5jkumb2.amplifyapp.com:3000")
                 .allowedMethods("GET", "POST")
                 .allowedHeaders("*")
                 .allowCredentials(true)
